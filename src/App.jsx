1c9abe6de24e7fdeb8866e4eb3129e65d48221ff
Damian <damigp2@gmail.com>
2024-01-31 20:29:45 -0300

    assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 import './App.css'
+const background=<img src='../src/resources/fondo.jpg' alt="fondo" className='Background'/>
+const Colors=["Aquamarine","Blueviolet","Azure","cyan","Tomato","lightgrey","greenyellow","lightsalmon","lightpink","skyblue"];
+
+
 
 function App() {
+  const [color,setColor]=useState("white");
+  const divStyle={backgroundColor:color};
   
   return (
-    <div>
-
+    <>
+    
+      <div className="heading">
+        {background}
+        <h1>DO YOU WANT TO REDESING YOUR OLD WALLS?!</h1>
+        <p>We have an extensive variety of colors from where you can pick and give new life to your old and boring walls</p>
+        <br/>
+        <h2>Take a look and pick what you like!</h2>
+      </div>
+      <div className="container">
+      <p>Test the colors with our pallet display</p>
+      <div className="card" style={divStyle}></div>
+      <p>The color you selected is: {color} <a href='#'>&#128722;</a></p>
+      <div className="buttons">
+      {Colors.map((colorName)=>(<button onClick={()=>
+      setColor(colorName)}
+      key={colorName} className="button">{colorName}</button>))}
+      </div>
     </div>
+    </>
   );
 }
 
